3c3db31a0705ce5ba0fb1eb27867c12fd042dbe3
MaxiCode <andresholsbach@gmail.com>
2025-06-09 23:01:22 -0300

    Se realizan modificaciones para presentacion

--- InteresConBucle.py
+++ InteresConBucle.py
@@ -5,7 +5,7 @@ Para simular el análisis de el algoritmo, generamos una lista con valores aleat
 Los datos de la inversion se asemejaran a una inversión de el plazo establecido.
 El resultado serán el valor final de la inversión y el tiempo que tardó en calcularlo, para cada iteración.
 """
-valoresIniciales = [642226.93, 760322.26, 104757.69, 399684.60, 395746.39, 740615.86, 959015.46, 854345.91, 725954.37, 165218.05]  # Valores iniciales de ejemplo
+valoresIniciales = [642226.93, 760322.26, 104757.69, 399684.60, 395746.39, 740615.86, 959015.46, 854345.91, 725954.37, 165218.05]  # Valores iniciales de ejemplo 
 
 for i in (valoresIniciales):  
     principal = i  # Inversión inicial de la lista
--- interesCompuesto.py
+++ interesCompuesto.py
@@ -5,12 +5,14 @@ Calculo de interes compuesto con bucle for
 Este programa calcula el interés compuesto de una inversión inicial
 con una tasa de interés anual y un número de años especificado.
 
+La T(n) = 1 + 2n, donde n es el número de periodos.
+Por lo tanto podemos decir que tiene una T(n) = O(n), es decir, lineal, lo que nos indica que el tiempo de ejecución aumenta linealmente con el número de periodos.
 """
 def valorFuturoIterativo(principal, tasaInteres, periodos):
     inicio = time.time()
-    valor = principal
+    valor = principal # 1 operación de asignación de valores
     for _ in range(periodos):
-        valor *= (1 + tasaInteres)
+        valor *= (1 + tasaInteres) # 2 operación sumatoria y multiplicación
     fin = time.time()
     return valor, (fin - inicio) * 1000  # Retorna el valor final y el tiempo en milisegundos
 
@@ -22,9 +24,11 @@ P = Capital inicial
 r = Tasa de interes anual
 n = Numero de periodos (años)
 VF = Valor futuro de la inversion
+La T(n) = 3, es decir, constante, lo que nos indica que el tiempo de ejecución no depende del número de periodos.
+Por lo tanto podemos decir que tiene una T(n) = O(1), es decir, constante.
 """
 def valorFuturoFormula(principal, tasaInteres, periodos):
     inicio = time.time()
-    valor = principal * (1 + tasaInteres) ** periodos
+    valor = principal * (1 + tasaInteres) ** periodos # 3 operaciones: 1 de multiplicación, 1 de suma y 1 de potencia
     fin = time.time()
     return valor, (fin - inicio) * 1000  # Retorna el valor final y el tiempo en milisegundos
